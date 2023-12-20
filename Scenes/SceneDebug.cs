ba58709fdacf76831dba0dedb54060c8cbea4702
PiotrDeda <piotrdeda.pl@gmail.com>
2023-12-20 22:45:08 +0100

    Added data loading from json

--- Scenes/SceneDebug.cs
+++ Scenes/SceneDebug.cs
@@ -1,10 +1,8 @@
-using JetBrains.Annotations;
+using Newtonsoft.Json;
 using Rokuro.Core;
+using Rokuro.Dtos;
 using Rokuro.Graphics;
-using Rokuro.MathUtils;
 using Rokuro.Objects;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
 
 namespace Toutetsu.Scenes;
 
@@ -15,30 +13,9 @@ public class SceneDebug : Scene
 		Name = "SceneDebug";
 		Camera = new(drawer, windowData);
 
-		var yamlObjects = new DeserializerBuilder()
-			.WithNamingConvention(UnderscoredNamingConvention.Instance)
-			.Build()
-			.Deserialize<List<YamlObjectModel>>(File.ReadAllText("assets/autogen/data/scenes/debug.yaml"));
-		foreach (YamlObjectModel objectModel in yamlObjects)
-			RegisterGameObject(objectModel.ToObject(Camera, spriteManager));
+		JsonConvert.DeserializeObject<SceneDto>(File.ReadAllText("assets/autogen/data/scenes/debug.json"))!
+			.GameObjects.ForEach(objectDto => RegisterGameObject(GameObject.FromDto(objectDto, Camera, spriteManager)));
 	}
 
 	Camera Camera { get; }
-
-	class YamlObjectModel
-	{
-		[UsedImplicitly] public string? Name { get; set; }
-		[UsedImplicitly] public string? TextureName { get; set; }
-		[UsedImplicitly] public string? Class { get; set; }
-		[UsedImplicitly] public int X { get; set; }
-		[UsedImplicitly] public int Y { get; set; }
-
-		public GameObject ToObject(Camera camera, SpriteManager spriteManager)
-		{
-			var o = (GameObject)Activator.CreateInstance(Type.GetType(Class!)!, new Vector2D(X, Y),
-				spriteManager.CreateSprite<StaticSprite>(TextureName!), camera)!;
-			o.Position = new(X, Y);
-			return o;
-		}
-	}
 }
